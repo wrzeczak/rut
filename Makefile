9ccb5b7780bfdd5d0a51247954edf67eb2e20551
Wrzeczak <wrzeczak@protonmail.com>
2023-09-11 22:08:33 -0500

    Create a Voronoi tesselation

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 run:
-	gcc src/main.c -lraylib -lm -g -Wall -Wextra -Wpedantic -Werror -O2 -o rut && ./rut
+	gcc src/main.c -lraylib -lm -g -Wall -Wextra -Wpedantic -Werror -O1 -o rut && ./rut
 
 experimental:
 	gcc src/experimental.c -lraylib -lm -g -O0 -o ex_rut && ./ex_rut
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <raylib.h>
+#include <raymath.h>
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
@@ -8,164 +9,178 @@
 
 const int HEIGHT = 900;
 const int WIDTH = 1600;
-const int CELLSIZE = 20;
+const int CELLSIZE = 10;
+
+const int MAPWIDTH = WIDTH / CELLSIZE;
+const int MAPHEIGHT = HEIGHT / CELLSIZE;
+
+#include "country_colors.h"
+const int COUNTRYMAX = COLOR_REGISTER_LENGTH;
 
 #define NOSAMPLE (Vector2) { -1, -1 }
 
-#define FIX_ATTEMPT_MAX 10000
+//------------------------------------------------------------------------------
 
-const int n = 2;
-float r; // such that the cell size (GRIDSIZE) can be 20
-int k = 30; // poisson generation depth
+typedef struct {
+	int x, y;
+	Color c;
+} Point;
 
 //------------------------------------------------------------------------------
 
 // returns zero if the two vectors are equal
-int Vector2_cmp(const Vector2 * a, const Vector2 * b) {
-	int c = ( a->x - b->x ) + ( a->y - b->y ); // if the two vectors are equal, this should be zero
-	return c;
+int Point_cmp(const Point * a, const Point * b) {
+	return (a->y == b->y && a->x == b->x) ? true : false;
 }
 
-void Vector2_drop(Vector2 * self) {
+void Point_drop(Point * self) {
 	// pretty sure nothing needs to be done here...
 	(void) self;
 }
 
 // clones a Vector2
-Vector2 Vector2_clone(Vector2 vec) {
-	Vector2 o = (Vector2) { vec.x, vec.y };
+Point Point_clone(Point vec) {
+	Point o = (Point) { vec.x, vec.y, vec.c };
 	return o;
 }
 
-// pvector = point vector, stores Vector2
+// pvector = point vector, stores Point
 #define i_type pvector
-#define i_keyclass Vector2
+#define i_keyclass Point
 #include "stc/cvec.h"
 // github.com/stclib/STC/blob/master/docs/cvec_api.md
 
 //------------------------------------------------------------------------------
 
-// Vector2 and pvector utility functions
-
-// generate a random point { ( 0 to domain_x_max ), ( 0 to domain_y_max ) }
-Vector2 random_point(int domain_x_max, int domain_y_max) {
-	return (Vector2) { (int) rand() % domain_x_max, (int) rand() % domain_y_max };
-}
-
-// return a vector of the grid position of a point
-Vector2 grid_position(Vector2 vec) {
-	return (Vector2) { floorf(vec.x / CELLSIZE), floorf(vec.y / CELLSIZE) };
-}
-
-// return the grid index of a point
-int grid_index(Vector2 vec, int grid_width, int grid_height) {
-	Vector2 v = grid_position(vec);
-	(void) grid_height; // wasn't technically needed, but included for whatever reason
-
-	return v.x + (v.y * grid_width);
+float distance(Point a, Point b) {
+	return sqrtf((float) (pow(a.x - b.x, 2) + pow(a.y - b.y, 2)));
 }
 
 //------------------------------------------------------------------------------
 
-pvector generate_random_points(int grid_width, int grid_height) {
-	pvector grid = { 0 };
+pvector generate_starting_countries() {
+	// int country_count = 16;
+	int country_grid_x = 8;
+	int country_grid_y = 2;
+	int grid_x_unit = floor(MAPWIDTH / country_grid_x);
+	int grid_y_unit = floor(MAPHEIGHT / country_grid_y);
 
-	int grid_size = grid_width * grid_height;
+	// country_grid_y * country_grid_x should equal country_count
 
-	// generate a bunch of random points inside each cell
-	for(int i = 0; i < (int) grid_size; i++) {
-		Vector2 inline_position = (Vector2) { rand() % CELLSIZE, rand() % CELLSIZE };
-		int grid_x = i % grid_width;
-		int grid_y = (i - (i % grid_width)) / grid_height;
+	pvector output = { 0 };
 
-		Vector2 position = (Vector2) { inline_position.x + (grid_x * CELLSIZE), inline_position.y + (grid_y * CELLSIZE) };
-		pvector_push(&grid, position);
+	pvector starting_points = { 0 };
+
+	// put a starting point in each grid cell
+	for(int gy = 0; gy < country_grid_y; gy++) {
+		for(int gx = 0; gx < country_grid_x; gx++) {
+			int x_offset = gx * grid_x_unit;
+			int y_offset = gy * grid_y_unit;
+
+			int x = rand() % grid_x_unit;
+			int y = rand() % grid_y_unit;
+
+			x += x_offset;
+			y += y_offset;
+
+			Point pnt = (Point) { x, y, country_color(gy * country_grid_x + gx) };
+
+			pvector_push(&starting_points, pnt);
+		}
 	}
 
-	for(int i = 0; i < grid_size; i++) {
-		Vector2 position = *(pvector_at(&grid, i));
+	// seeds points should be relatively evenly spaced out while still having countries of varying sizes
 
-		for(int gx = -1; gx < 2; gx++) {
-			for(int gy = -1; gy < 2; gy++) {
-				int i_translate = gx + (gy * grid_width);
-				bool position_fixed = false;
-				if(i + i_translate >= 0 && (i + i_translate) < grid_size) {
-					Vector2 neighbor = *(pvector_at(&grid, i + i_translate));
+	for(int i = 0; i < (int) pvector_size(&starting_points); i++) {
+		pvector_push(&output, *(pvector_at(&starting_points, i)));
+	}
 
-					for(int j = 0; j < FIX_ATTEMPT_MAX; j++) {
-						float distance = sqrtf(pow(neighbor.x - position.x, 2) + pow(neighbor.y - position.y, 2));
+	for(int y = 0; y < MAPHEIGHT; y++) {
+		for(int x = 0; x < MAPWIDTH; x++) {
+			Point pnt = (Point) { x, y, country_color(-1) };
 
-						if(distance < r) {
-							Vector2 inline_position = (Vector2) { rand() % CELLSIZE, rand() % CELLSIZE };
-							int grid_x = i % grid_width;
-							int grid_y = (i - (i % grid_width)) / grid_height;
+			float shortest_distance = distance((Point) { 0, 0, country_color(-1) }, (Point) { MAPWIDTH, MAPHEIGHT, country_color(-1) });
 
-							position = (Vector2) { inline_position.x + (grid_x * CELLSIZE), inline_position.y + (grid_y * CELLSIZE) };
-						} else {
-							position_fixed = true;
-						}
-					}
+			for(int j = 0; j < (int) pvector_size(&starting_points); j++) {
 
-					if(position_fixed)
-						grid.data[i] = position;
-					else
-						grid.data[i] = NOSAMPLE;
+				Point ref = *(pvector_at(&starting_points, j));
+				float d = distance(pnt, ref);
+
+				if(d < shortest_distance) {
+					pnt.c = country_color(j);
+					shortest_distance = d;
+				}
+			}
+
+			pvector_push(&output, pnt);
+		}
+	}
+
+	pvector sorted_output = { 0 };
+
+	// comically inefficient, but it'll get the job done for now
+	for(int y = 0; y < MAPHEIGHT; y++) {
+		for(int x = 0; x < MAPWIDTH; x++) {
+			for(int i = 0; i < (int) pvector_size(&output); i++) {
+				Point pnt = *(pvector_at(&output, i));
+
+				if(pnt.x == x && pnt.y == y) {
+					pvector_push(&sorted_output, pnt);
 				}
 			}
 		}
 	}
 
-	return grid;
+
+	return sorted_output;
 }
 
+
+//------------------------------------------------------------------------------
+
+void render_pvector(pvector points) {
+	for(int i = 0; i < (int) pvector_size(&points); i++) {
+		Point pnt = *(pvector_at(&points, i));
+
+		if(pnt.x != -1 && pnt.y != -1) DrawPixel(pnt.x, pnt.y, pnt.c);
+	}
+}
 
 //------------------------------------------------------------------------------
 
 int main(void) {
+	pvector map = generate_starting_countries();
 
-	r = CELLSIZE * sqrt(n); // as previously defined
-	pvector poisson_points = generate_random_points(WIDTH / CELLSIZE, HEIGHT / CELLSIZE);
-	int poisson_point_count = pvector_size(&poisson_points);
+	InitWindow(WIDTH, HEIGHT, "Fanter!");
 
-	printf("Poisson points are finished generating!\n");
-
-	InitWindow(WIDTH, HEIGHT, "The Game of all time");
-	SetTargetFPS(150);
-
-	RenderTexture2D target = LoadRenderTexture(WIDTH, HEIGHT);
+	RenderTexture2D tx = LoadRenderTexture(MAPWIDTH, MAPHEIGHT);
 
 	while(!WindowShouldClose()) {
 
-		if(IsKeyPressed(KEY_SPACE)) {
-			pvector_clear(&poisson_points);
-			poisson_points = generate_poisson_points(WIDTH / CELLSIZE, HEIGHT / CELLSIZE);
-			poisson_point_count = pvector_size(&poisson_points);
-			printf("Poisson points regenerated!\n");
-		}
+		Vector2 real_mp = (Vector2) { GetMouseX(), GetMouseY() };
+		Vector2 grid_mp = (Vector2) { floor(real_mp.x / CELLSIZE), floor(real_mp.y / CELLSIZE) };
 
-		BeginTextureMode(target);
-			ClearBackground(BLACK);
-
-			for(int i = 0; i < poisson_point_count; i++) {
-				Vector2 px = poisson_points.data[i];
-				if(px.x != -1 && px.y != -1) {
-					DrawPixelV(px, RED);
-				}
-			}
-
+		BeginTextureMode(tx);
+			render_pvector(map);
 		EndTextureMode();
 
 		BeginDrawing();
 			ClearBackground(BLACK);
 
-			DrawTexture(target.texture, 0, 0, WHITE);
+			DrawTextureEx(tx.texture, (Vector2) { 0, 0 }, 0.0f, (float) CELLSIZE, WHITE);
 
 			DrawFPS(10, 10);
+
+			DrawText(TextFormat("[%d, %d] [%d, %d]", grid_mp.x, grid_mp.y, real_mp.x, real_mp.y), 10, 40, 20, RAYWHITE);
 		EndDrawing();
+
 	}
+
+	UnloadRenderTexture(tx);
 
 	CloseWindow();
 
 	return 0;
+
 }
 
